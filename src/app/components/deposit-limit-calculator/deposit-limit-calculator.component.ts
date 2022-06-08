b471017050e75e6271fe3f52c382e0d380a57f72
Cillian O Murchu <cillian.omurchu@betssongroup.com>
2022-06-08 14:04:38 +0200

    By amount ux

--- src/app/components/deposit-limit-calculator/deposit-limit-calculator.component.ts
+++ src/app/components/deposit-limit-calculator/deposit-limit-calculator.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-deposit-limit-calculator',
   templateUrl: './deposit-limit-calculator.component.html',
-  styleUrls: ['./deposit-limit-calculator.component.scss']
+  styleUrls: ['./deposit-limit-calculator.component.scss'],
 })
 export class DepositLimitCalculatorComponent implements OnInit {
-
-  constructor() { }
+  displayResult: boolean | undefined;
+  calculatedResult = 4000;
+  constructor() {}
 
   ngOnInit(): void {
+    this.displayResult = true;
   }
 
+  onCalculate(boo: boolean) {
+    this.displayResult = boo;
+  }
+
+  onRemindMe() {
+    console.log('calculate');
+  }
 }

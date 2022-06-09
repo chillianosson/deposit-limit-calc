b371c20af33dd150119323b76674b441b0bfd9a9
Cillian O Murchu <cillian.omurchu@betssongroup.com>
2022-06-09 17:27:52 +0200

    change primary color

--- src/app/components/deposit-limit-calculator-container/dep-limit-calc/dep-limit-calc.component.ts
+++ src/app/components/deposit-limit-calculator-container/dep-limit-calc/dep-limit-calc.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component, HostBinding, OnInit, ViewEncapsulation } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 import { DepositLimitCalculatorResult, InformationalTooltip } from "./dep-limit-calc.model";
 
@@ -6,17 +7,33 @@ import { DepositLimitCalculatorResult, InformationalTooltip } from "./dep-limit-
   selector: 'app-dep-limit-calc',
   templateUrl: './dep-limit-calc.component.html',
   styleUrls: ['./dep-limit-calc.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class DepLimitCalcComponent implements OnInit {
+  currency = { value: 'SEK' };
   result: DepositLimitCalculatorResult | undefined;
   displayResult = false;
   informationalTooltip = InformationalTooltip;
-  constructor() {}
+
+  formGroup: FormGroup = this.formBuilder.group({
+    amount: this.formBuilder.control(''),
+    date: this.formBuilder.control(''),
+    time: this.formBuilder.control(''),
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {}
 
   onCalculate(): void {
     console.log('onCalculate called');
+
+    const data = {
+      amount: this.formGroup.controls.amount.value,
+      date: this.formGroup.controls.date.value,
+      time: this.formGroup.controls.time.value,
+    };
   }
 
   onRemindMe(): void {
